7dfd6b9e7f645602d35af7982cab0f9528c62fa1
Emmanuel Lelei <manuellelei750@gmail.com>
2024-10-08 16:27:29 +0300

    Error handling on the search bar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // paste your secret key
 const api = {
-    key: "d8973****************", // your secrete key here
+    key: "d8973e08f2ba0e8f02819e50c56bc08a", // your secrete key here
     base_url: "https://api.openweathermap.org/data/2.5/",
 }
 
@@ -11,72 +11,85 @@ const searchbox = document.querySelector(".search-box");
 searchbox.addEventListener("keydown", setQuery);
 
 // when user enter city and and click the enter key then getResults function will invoked
+// Set query when Enter key is pressed
 function setQuery(e) {
-    // 13 is key code for enter key
+    // 13 is keycode for Enter key
     if (e.keyCode == 13) {
-        // calling getResults function and passing city name when click enter key
-        console.log("Fetching weather for:", searchbox.value); 
-        getResults(searchbox.value);
-        // console.log(searchbox.value);
+        console.log("Fetching weather for:", searchbox.value);
+        getResults(searchbox.value); // Fetch results when Enter is pressed
     }
 }
 
-// fetching the data from weather api
+// Fetching the data from the weather API
 function getResults(query) {
-    // qeury carries city name and we passes to the Api url so final url be looks like this 
-    // https://api.openweathermap.org/data/2.5/weather?q=London&unit=metric&APPID=9f9189f3ea73cf55b8cc02b2d5091f72
-
-    //so query passes to this URL to get data for the user-entered city name
-
+    // Fetching weather data from OpenWeatherMap API
     fetch(`${api.base_url}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => weather.json())
-    .then(displayResults)
-    .catch(error => {
-        console.error("Error fetching data: ", error);
-        alert("City not found or network issue!");
-    }); // passing response (weather) object
+        .then(response => response.json()) // Convert response to JSON
+        .then(weather => {
+            if (weather.cod === '404') {
+                // Display error message if city is not found
+                displayError("City not found. Please try again.");
+            } else {
+                // Display weather data if successful
+                displayResults(weather);
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching data: ", error);
+            displayError("Network issue or city not found!");
+        });
 }
 
-
-
+// Function to display weather results on the page
 function displayResults(weather) {
-    // console.log(typeof weather);
-
-    // accessing weather object and getting data
-
-    // selecting the element and setting the city and country name from api
+    // Selecting the elements and setting city and country
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-    // setting current date, see the dateBuilder() function bellow
+    // Setting the current date using dateBuilder function
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
 
-    // selecting the element and setting the current temperature of city
+    // Setting the current temperature of the city
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-    // selecting the element and setting the current weather of city like, 'sunny', 'cloudy'
+    // Setting the current weather condition (e.g., 'Sunny', 'Cloudy')
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
-    // selecting the element and setting the current weather description of city
+    // Setting the weather description (e.g., 'clear sky', 'light rain')
     let weather_description = document.querySelector('.current .description');
     weather_description.innerText = weather.weather[0].description;
 
-    // selecting the element and setting the current weather image/icon of city
+    // Setting the weather icon based on current conditions
     let weather_icon = document.querySelector('.current .weather-icon img');
-    //console.log(weather.weather[0].icon);
     weather_icon.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`;
 
-    // selecting the element and setting the current min and max temperature of city
+    // Setting the min and max temperature
     let hilow = document.querySelector('.current .hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 
+    // Clear any previous error message when results are displayed
+    clearError();
 }
 
-// returns today's date
+// Display an error message on the page
+function displayError(message) {
+    let errorElement = document.querySelector('.location .city');
+    errorElement.innerText = message;
+}
+
+// Clear the error message when valid results are displayed
+function clearError() {
+    let errorElement = document.querySelector('.location .city');
+    if (errorElement.innerText.includes("not found")) {
+        errorElement.innerText = ''; // Clear the error message
+    }
+}
+
+// Returns today's date in a readable format
 function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -88,3 +101,4 @@ function dateBuilder(d) {
 
     return `${day}, ${date} ${month} ${year}`;
 }
+
